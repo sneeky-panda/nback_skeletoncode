4c94e089201ab450351580d0f804f6836f055d70
sneeky-panda <127853393+sneeky-panda@users.noreply.github.com>
2024-11-06 12:36:59 +0100

    Gridd på andra skämen med inputt

--- app/src/main/java/mobappdev/example/nback_cimpl/ui/screens/GameScreen.kt
+++ app/src/main/java/mobappdev/example/nback_cimpl/ui/screens/GameScreen.kt
@@ -1,12 +1,23 @@
 package mobappdev.example.nback_cimpl.ui.screens
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarDuration
 import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
@@ -17,8 +28,12 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import kotlinx.coroutines.launch
+import mobappdev.example.nback_cimpl.R
 import mobappdev.example.nback_cimpl.ui.viewmodels.GameViewModel
 
 @Composable
@@ -41,9 +56,10 @@ fun GameScreen(vm: GameViewModel, nav: () -> Unit) {
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
             Text(
-                text = "Game Screen",
+                text = "N-back Game",
                 style = MaterialTheme.typography.headlineLarge,
                 modifier = Modifier.padding(16.dp)
+
             )
 
             // Visa aktuell poäng
@@ -53,13 +69,39 @@ fun GameScreen(vm: GameViewModel, nav: () -> Unit) {
                 modifier = Modifier.padding(16.dp)
             )
 
-            // Visa spelstatus eller eventValue från gameState
-            if (gameState.eventValue != -1) {
-                Text(
-                    text = "Current Event Value: ${gameState.eventValue}",
-                    style = MaterialTheme.typography.bodyLarge,
-                    modifier = Modifier.padding(16.dp)
-                )
+            //3x3
+            Column( verticalArrangement = Arrangement.spacedBy(8.dp),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                modifier = Modifier.padding(16.dp)
+            ){
+                for(i in 0 until 3){
+                    Row(
+                        horizontalArrangement = Arrangement.spacedBy(8.dp)
+                    ){
+                        for(j in 0 until 3){
+                            val position = i *3+ j
+                            Box(
+                                modifier = Modifier
+                                    .size(80.dp)
+                                    .border(2.dp, Color.Black)
+                                    .background(
+                                        if(gameState.eventValue ==position) Color.Green else Color.Transparent
+                                    ).clickable {
+                                        scope.launch {
+                                            val isCorret = vm.processUserAction(position)
+                                            snackBarHostState.showSnackbar(
+                                                if(isCorret) "Correct!" else "incorrect!"
+                                            )
+                                        }
+                                    }, contentAlignment = Alignment.Center
+                            ){ Text(
+                                text = position.toString(),
+                                style = MaterialTheme.typography.bodyLarge
+                            ) }
+
+                        }
+                    }
+                }
             }
 
 
@@ -69,6 +111,60 @@ fun GameScreen(vm: GameViewModel, nav: () -> Unit) {
                 modifier = Modifier.padding(top = 32.dp)
             ) {
                 Text(text = "Back to Home")
+            }
+            if (gameState.eventValue != -1) {
+                Text(
+                    modifier = Modifier.fillMaxWidth(),
+                    text = "Current eventValue is: ${gameState.eventValue}",
+                    textAlign = TextAlign.Center
+                )
+            }
+            Button(onClick = vm::startGame) {
+                Text(text = "Generate eventValues")
+            }
+
+
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp),
+                horizontalArrangement = Arrangement.SpaceAround,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Button(onClick = {
+                    // Todo: change this button behaviour
+                    scope.launch {
+                        snackBarHostState.showSnackbar(
+                            message = "Hey! you clicked the audio button"
+                        )
+                    }
+                }) {
+                    Icon(
+                        painter = painterResource(id = R.drawable.sound_on),
+                        contentDescription = "Sound",
+                        modifier = Modifier
+                            .height(48.dp)
+                            .aspectRatio(3f / 2f)
+                    )
+                }
+                Button(
+                    onClick = {
+                        // Todo: change this button behaviour
+                        scope.launch {
+                            snackBarHostState.showSnackbar(
+                                message = "Hey! you clicked the visual button",
+                                duration = SnackbarDuration.Short
+                            )
+                        }
+                    }) {
+                    Icon(
+                        painter = painterResource(id = R.drawable.visual),
+                        contentDescription = "Visual",
+                        modifier = Modifier
+                            .height(48.dp)
+                            .aspectRatio(3f / 2f)
+                    )
+                }
             }
         }
     }
